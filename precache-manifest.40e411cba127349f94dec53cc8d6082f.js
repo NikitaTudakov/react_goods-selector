22564481fefa02450377f0fca6c02f2b41a5962f
NikitaTudakov <tudakov.nikita@gmail.com>
2021-01-17 15:43:37 +0200

    Updates

---
+++ precache-manifest.40e411cba127349f94dec53cc8d6082f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfd486b82029a6060e05cf3a8b15a87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b970e67452c631e215c3",
+    "url": "./static/css/main.913da521.chunk.css"
+  },
+  {
+    "revision": "4b82e77d18a6602aeda2",
+    "url": "./static/js/2.7c2887fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7c2887fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b970e67452c631e215c3",
+    "url": "./static/js/main.5c897b7a.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
